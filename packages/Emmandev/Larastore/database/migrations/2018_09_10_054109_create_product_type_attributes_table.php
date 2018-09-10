73447093f9ea801d2bdc29ed271515ac0bdf1590
Emmanuel Tierra <emman.tierra@gmail.com>
2018-09-10 14:05:05 +0800

    Create product type attributes migration

---
+++ packages/Emmandev/Larastore/database/migrations/2018_09_10_054109_create_product_type_attributes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTypeAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_type_attributes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_type_id')->unsigned();
+            $table->foreign('product_type_id')->references('id')->on('product_types')->onDelete('cascade');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_type_attributes');
+    }
+}
