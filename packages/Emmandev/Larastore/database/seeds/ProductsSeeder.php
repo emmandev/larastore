a0ad09b2daec0df5a6194295e1a92540eb4eb340
Emmanuel Tierra <emman.tierra@gmail.com>
2018-09-13 11:26:30 +0800

    Update seeder products relationships

--- packages/Emmandev/Larastore/database/seeds/ProductsSeeder.php
+++ packages/Emmandev/Larastore/database/seeds/ProductsSeeder.php
@@ -30,10 +30,10 @@ class ProductsSeeder extends Seeder
             $types = $productTypes->random(rand(1, 5));
 
             $types->each(function ($type) use ($product){
-                $product->productAtrributes()->attach($type->attributes()->pluck('id')->toArray());
+                $product->atrributes()->attach($type->attributes()->pluck('id')->toArray());
             });
 
-            $product->productTypes()->attach($types->pluck('id')->toArray());
+            $product->types()->attach($types->pluck('id')->toArray());
         });
     }
 }
