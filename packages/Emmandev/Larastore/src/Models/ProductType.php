e9b0036d2b63dd58fe0cdb01fbcfe4fd76d8ab16
Emmanuel Tierra <emman.tierra@gmail.com>
2018-09-13 10:59:11 +0800

    Set timestamps to false

--- packages/Emmandev/Larastore/src/Models/ProductType.php
+++ packages/Emmandev/Larastore/src/Models/ProductType.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductType extends Model
 {
+    public $timestamps = false;
+
     public function products()
     {
         return $this->belongsToMany('App\Product')
