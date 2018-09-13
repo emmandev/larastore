cbe0fb1582450efb4971f51ab51b498af1345387
Emmanuel Tierra <emman.tierra@gmail.com>
2018-09-13 11:10:55 +0800

    Update product factories namespace

--- packages/Emmandev/Larastore/database/factories/ProductFactory.php
+++ packages/Emmandev/Larastore/database/factories/ProductFactory.php
@@ -2,7 +2,7 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Product::class, function (Faker $faker) {
+$factory->define(App\Models\Product::class, function (Faker $faker) {
     $name = $faker->sentence(3);
 
     return [
--- packages/Emmandev/Larastore/database/factories/ProductTypeAttributeFactory.php
+++ packages/Emmandev/Larastore/database/factories/ProductTypeAttributeFactory.php
@@ -2,7 +2,7 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\ProductTypeAttribute::class, function (Faker $faker) {
+$factory->define(App\Models\ProductTypeAttribute::class, function (Faker $faker) {
     $name = $faker->sentence(3);
 
     return [
