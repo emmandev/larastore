0cff07089457f8ce23b43b46aae6e7b4a0b51ce6
Unknown <emman.tierra@gmail.com>
2018-09-17 16:19:45 +0800

    Change route to having relational data

--- packages/Emmandev/Larastore/src/routes.php
+++ packages/Emmandev/Larastore/src/routes.php
@@ -1,7 +1,7 @@
 <?php
 
-Route::get('products/{product}', function ($product) {
+Route::get('products/{product}/{relate}', function ($product, $relate) {
     $product = App\Models\Product::findOrFail($product);
 
-    return $product->metas;
+    return $product->{$relate};
 });
