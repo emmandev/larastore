faf9998b3548cd33140b1d776281edd015d1527a
Unknown <emman.tierra@gmail.com>
2018-09-18 14:24:59 +0800

    Create product factory

---
+++ packages/Emmandev/Larastore/database/factories/ProductFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Models\Product::class, function (Faker $faker) {
+    $name = $faker->sentence(3);
+    $slug = str_slug($name);
+    return [
+        'name' => $name,
+        'description' => $faker->text(),
+        'sku' => $faker->bothify('sku-' . $slug . '-##??##?'),
+        'slug' => $slug,
+        'price' => $faker->numberBetween(100, 5000),
+        'stock' => $faker->numberBetween(0, 50),
+        'status' => $faker->numberBetween(0, 2)
+    ];
+});
