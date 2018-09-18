095159c89178dbfda76591d4b91b864ba10d1813
Unknown <emman.tierra@gmail.com>
2018-09-18 11:32:04 +0800

    Rename product type attribute model

--- packages/Emmandev/Larastore/src/Models/ProductTypeMeta.php
+++ packages/Emmandev/Larastore/src/Models/ProductTypeMeta.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ProductTypeAttribute extends Model
+class ProductTypeMeta extends Model
 {
     public $timestamps = false;
 
