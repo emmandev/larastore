cb0eb282b25e0f5c8f9f7ffd4187cabd25cc8ea2
Unknown <emman.tierra@gmail.com>
2018-09-20 11:16:41 +0800

    Update class name for migration

--- packages/Emmandev/Larastore/database/migrations/2018_09_10_054109_create_product_type_metas_table.php
+++ packages/Emmandev/Larastore/database/migrations/2018_09_10_054109_create_product_type_metas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateProductTypeAttributesTable extends Migration
+class CreateProductTypeMetasTable extends Migration
 {
     /**
      * Run the migrations.
