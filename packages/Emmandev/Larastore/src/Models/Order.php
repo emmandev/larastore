cd2ffd9be960390f68e3b5fc9abf8f107df7ddfc
Emmanuel Tierra <emman.tierra@gmail.com>
2018-09-21 15:15:00 +0800

    Add user model relationship for orders

--- packages/Emmandev/Larastore/src/Models/Order.php
+++ packages/Emmandev/Larastore/src/Models/Order.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    public function user()
+    {
+        return $this->belongsTo('App\User\User');
+    }
+
     public function products()
     {
         return $this->belongsToMany('App\Models\Product')
