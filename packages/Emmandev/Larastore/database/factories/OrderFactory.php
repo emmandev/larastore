848d6427edc10b1729441eebff58aed058aad736
Emmanuel Tierra <emman.tierra@gmail.com>
2018-09-21 17:00:50 +0800

    Add default user_id field for order factory

--- packages/Emmandev/Larastore/database/factories/OrderFactory.php
+++ packages/Emmandev/Larastore/database/factories/OrderFactory.php
@@ -4,6 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Order::class, function (Faker $faker) {
     return [
+        'user_id' => function () {
+            return App\Models\User::inRandomOrder()->first()->id;
+        },
         'status' => $faker->numberBetween(1, 3)
     ];
 });
