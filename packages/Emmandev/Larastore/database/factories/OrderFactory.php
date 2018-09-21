ae97b07712158a7360da6e4f013002575efe5280
Emmanuel Tierra <emman.tierra@gmail.com>
2018-09-21 15:20:27 +0800

    Remove user_id field in order factory

--- packages/Emmandev/Larastore/database/factories/OrderFactory.php
+++ packages/Emmandev/Larastore/database/factories/OrderFactory.php
@@ -4,7 +4,6 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Order::class, function (Faker $faker) {
     return [
-        'user_id' => $faker->numberBetween(1, 50),
         'status' => $faker->numberBetween(1, 3)
     ];
 });
