8b340e529e0a04dfe4010effdfc71daf85bd424c
Emmanuel Tierra <emman.tierra@gmail.com>
2018-09-24 14:56:10 +0800

    Create order model

---
+++ packages/Emmandev/Larastore/src/Models/Order.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany('App\Models\Product')
+                    ->using('App\Models\OrderProduct')
+                    ->withPivot('price', 'quantity', 'total_price');
+    }
+
+    public function metas()
+    {
+        return $this->hasMany('App\Models\OrderMeta');
+    }
+}
