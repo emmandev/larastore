90e0ff766e5e2eab85b17daf0d51a05d4929de64
Emmanuel Tierra <emman.tierra@gmail.com>
2018-09-28 14:36:22 +0800

    Add meta, products, and user data to order resource

--- packages/Emmandev/Larastore/src/Resources/Order.php
+++ packages/Emmandev/Larastore/src/Resources/Order.php
@@ -3,6 +3,9 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\OrderMetaCollection;
+use App\Http\Resources\OrderProductCollection;
+use App\Http\Resources\User as UserResource;
 
 class Order extends JsonResource
 {
@@ -15,7 +18,23 @@ class Order extends JsonResource
     public function toArray($request)
     {
         return [
-            'status' => $this->status
+            'id' => $this->id,
+            'status' => $this->status,
+        ];
+    }
+
+    /**
+     * Get additional data that should be returned with the resource array.
+     *
+     * @param \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function with($request)
+    {
+        return [
+            'metas' => OrderMetaCollection::make($this->metas),
+            'products' => OrderProductCollection::make($this->products),
+            'user' => new UserResource($this->user)
         ];
     }
 }
