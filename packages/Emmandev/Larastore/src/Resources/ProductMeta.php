bef64e8bebc54ea60e6439219d631a3c1ffcc9fe
Emmanuel Tierra <emman.tierra@gmail.com>
2018-10-03 11:38:02 +0800

    Add id field to product meta resource

--- packages/Emmandev/Larastore/src/Resources/ProductMeta.php
+++ packages/Emmandev/Larastore/src/Resources/ProductMeta.php
@@ -15,6 +15,7 @@ class ProductMeta extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'slug' => $this->slug,
             'key' => $this->name,
