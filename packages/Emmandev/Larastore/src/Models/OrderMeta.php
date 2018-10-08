9651fc4b53627e1b56e7a8d2f8dcb1580c8372e6
Emmanuel Tierra <emman.tierra@gmail.com>
2018-10-08 15:51:41 +0800

    Add method to return order meta keys; set time stamp to false

--- packages/Emmandev/Larastore/src/Models/OrderMeta.php
+++ packages/Emmandev/Larastore/src/Models/OrderMeta.php
@@ -6,8 +6,67 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderMeta extends Model
 {
+    public $timestamps = false;
+
     public function order()
     {
         return $this->belongsTo('App\Models\Order');
+    }
+
+    public function orderMetaKeys()
+    {
+        return [
+            'customer' => [
+                'name',
+                'email',
+                'phone',
+                'company',
+                'ip_address',
+                'user_agent',
+            ],
+
+            'billing' => [
+                'billing_name',
+                'billing_address_1',
+                'billing_address_2',
+                'billing_city',
+                'billing_state',
+                'billing_post_code',
+                'billing_country',
+            ],
+
+            'shipping' => [
+                'shipping_name',
+                'shipping_address_1',
+                'shipping_address_2',
+                'shipping_city',
+                'shipping_state',
+                'shipping_post_code',
+                'shipping_country',
+                'shipping_price',
+                'shipping_tax',
+                'shipping_discount',
+                'shipping_method'
+            ],
+
+            'cart' => [
+                'cart_discount',
+                'cart_discount_tax',
+                'cart_quantity',
+                'cart_total',
+                'notes',
+                'coupon_code',
+                'coupon_discount',
+            ],
+
+            'payment' => [
+                'total_tax',
+                'total_price',
+                'payment_method',
+                'payment_date',
+                'currency'
+            ],
+
+        ];
     }
 }
