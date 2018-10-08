e59d64793bb0edb22c628ee288e046c9d80c699c
Emmanuel Tierra <emman.tierra@gmail.com>
2018-10-08 14:41:08 +0800

    Add accessor and mutator for order product model

--- packages/Emmandev/Larastore/src/Models/OrderProduct.php
+++ packages/Emmandev/Larastore/src/Models/OrderProduct.php
@@ -4,8 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
+use App\Http\Traits\MoneyTrait;
+
 class OrderProduct extends Pivot
 {
+    use MoneyTrait;
+
     public function metas()
     {
         return $this->hasMany('App\Models\OrderProductMeta', 'order_product_id');
@@ -14,5 +18,37 @@ class OrderProduct extends Pivot
     public function product()
     {
         return $this->belongsTo('App\Models\Product');
+    }
+
+    /**
+     * Formats the price attribute of the model after retrieval from the database
+     */
+    public function getPriceAttribute($value)
+    {
+        return $this->formatToFloat($value);
+    }
+
+    /**
+     * Formats the price attribute of the model before saving to the database
+     */
+    public function setPriceAttribute($value)
+    {
+        $this->attributes['price'] = $this->formatToInt($price);
+    }
+
+    /**
+     * Formats the price attribute of the model after retrieval from the database
+     */
+    public function getTotalPriceAttribute($value)
+    {
+        return $this->formatToFloat($value);
+    }
+
+    /**
+     * Formats the price attribute of the model before saving to the database
+     */
+    public function setTotalPriceAttribute($value)
+    {
+        $this->attributes['total_price'] = $this->formatToInt($price);
     }
 }
